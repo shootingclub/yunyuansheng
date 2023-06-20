25a9f663129c04ac402b8112e2dab35dcec7ffd5
张耀华 <13752354704@163.com>
2023-06-20 12:11:34 +0800

    module-2 task

---
+++ module2/2.1.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", handleInterceptor(httpAccessFunc))
+	http.HandleFunc("/health", handleInterceptor(healthFunc))
+	err := http.ListenAndServe(":8999", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthFunc(w http.ResponseWriter, r *http.Request) {
+	HealthCode := "200"
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(HealthCode))
+	return
+}
+
+func httpAccessFunc(w http.ResponseWriter, r *http.Request) {
+	//request header写入response header
+	//if len(r.Header) > 0 {
+	//	for k, v := range r.Header {
+	//		log.Printf("%s=%s", k, v[0])
+	//		w.Header().Set(k, v[0])
+	//	}
+	//}
+	//
+	//// 设置环境值的值
+	//os.Setenv("VERSION", "GOLANG YUN YUAN SHENG")
+	//// 获取环境变量"VERSION"
+	//name := os.Getenv("VERSION")
+	//log.Printf("VERSION ENV : %s \n", name)
+	//
+	////获取Client IP，并且打印出来
+	//ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	//if err != nil {
+	//	fmt.Println("err:", err)
+	//}
+	//
+	//if net.ParseIP(ip) != nil {
+	//	log.Printf("ip : %s\n", ip)
+	//}
+	//log.Printf("http Status Code : %d \n", http.StatusOK)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Server Run Success "))
+
+	return
+}
+
+func handleInterceptor(h http.HandlerFunc) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		//request header写入response header
+		if len(r.Header) > 0 {
+			for k, v := range r.Header {
+				log.Printf("%s=%s", k, v[0])
+				w.Header().Set(k, v[0])
+			}
+		}
+
+		// 设置环境值的值
+		os.Setenv("VERSION", "GOLANG YUN YUAN SHENG")
+		// 获取环境变量"VERSION"
+		name := os.Getenv("VERSION")
+		log.Printf("VERSION ENV : %s \n", name)
+
+		//获取Client IP，并且打印出来
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			fmt.Println("err:", err)
+		}
+
+		if net.ParseIP(ip) != nil {
+			log.Printf("ip : %s\n", ip)
+		}
+		log.Printf("http Status Code : %d \n", http.StatusOK)
+
+		h(w, r)
+	}
+
+}
---
+++ module2/work2.md
@@ -0,0 +1,4 @@
+接收客户端 request，并将 request 中带的 header 写入 response header
+读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
+当访问 localhost/healthz 时，应返回 200
