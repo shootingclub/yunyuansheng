dbd93fe6a136b78455fcb7ea27ee9493ce22bb55
张耀华 <13752354704@163.com>
2023-06-21 12:27:48 +0800

    module-1 task

--- module1/1.2.go
+++ module1/1.2.go
@@ -3,40 +3,62 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
-	Queue()
+	QueueTest()
 }
 
-func Queue() {
-	queue := make(chan int, 10)
-	go produce(queue)
-	consumer(queue)
+type Queue struct {
+	q    chan int
+	cond *sync.Cond
 }
 
-func produce(queue chan int) {
-	defer func() {
-		if r := recover(); r == nil {
-			fmt.Errorf("%s", "produce to panic")
-			produce(queue)
-		}
-	}()
-	ticker := time.NewTicker(time.Second * 1)
+func QueueTest() {
+	queue := Queue{
+		q:    make(chan int, 10),
+		cond: sync.NewCond(&sync.Mutex{}),
+	}
+
+	go queue.consumer()
 	for {
-		select {
-		case <-ticker.C:
-			rand.Seed(time.Now().UnixNano())
-			data := rand.Intn(100)
-			queue <- data
-			fmt.Println("生产者", data)
-		}
+		time.Sleep(time.Second)
+		rand.Seed(time.Now().UnixNano())
+		data := rand.Intn(100)
+		queue.produce(data)
+	}
+
+}
+
+func (queue *Queue) produce(item int) {
+	queue.cond.L.Lock()
+	defer queue.cond.L.Unlock()
+	// 开始入队
+	queue.q <- item
+	fmt.Println("生产者", item)
+	// 入队进行判断是否队列已满
+	if len(queue.q) == cap(queue.q) {
+		fmt.Println("生产完成 开始消费")
+		queue.cond.Wait()
 	}
 }
-func consumer(queue chan int) {
-	for data := range queue {
-		fmt.Println("消费者", data)
-		time.Sleep(time.Second)
+
+func (queue *Queue) consumer() {
+	for {
+		// 是否满了
+		if cap(queue.q) == len(queue.q) {
+			for data := range queue.q { //消费
+				if len(queue.q) != 0 {
+					time.Sleep(time.Second)
+					fmt.Println("消费者", data)
+				} else { // 消费完了
+					fmt.Println("消费完成 开始生产")
+					queue.cond.Signal()
+					break
+				}
+			}
+		}
 	}
 }
