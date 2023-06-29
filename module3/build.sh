dc9f7fc6ce1afa4424ef5e1f96208f81ce3ecee5
张耀华 <13752354704@163.com>
2023-06-29 18:17:54 +0800

    module-3 task

--- module3/build.sh
+++ module3/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+set -e
+username=mock_user
+password=mock_password
+
 
 echo "pull project yunyuansheng"
 git clone git@github.com:shootingclub/yunyuansheng.git
@@ -7,5 +11,7 @@ echo "build images"
 docker build -t httpserver:last .
 echo "image list"
 docker images
+echo "push images to docker repo"
+docker login -u $username -p $password registry.hub.docker.com && docker push registry.hub.docker.com/httpserver:last
 echo "run container"
 docker run -p 8999:8999 --name httpserver -d httpserver:last
